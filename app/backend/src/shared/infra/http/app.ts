b224db1e038f71bdc4760253c99738046c4c1717
Saggioro <diegosaggiorobrito@gmail.com>
2020-05-28 22:00:01 -0300

    importações no singular + importação do container no app

--- app/backend/src/shared/infra/http/app.ts
+++ app/backend/src/shared/infra/http/app.ts
@@ -1,10 +1,13 @@
 import 'reflect-metadata';
+import 'express-async-errors';
+import express, { Response, Request, NextFunction } from 'express';
 
-import express from 'express';
+import AppError from '@shared/errors/AppError';
 import uploadConfig from '@config/upload';
 import routes from './routes';
 
 import '@shared/infra/typeorm';
+import '@shared/container';
 
 const app = express();
 
@@ -13,5 +16,20 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+        return response
+            .status(err.statusCode)
+            .json({ status: 'error', message: err.message });
+    }
+
+    console.error(err);
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+    });
+});
 
 export default app;
--- app/backend/src/shared/infra/http/routes/index.ts
+++ app/backend/src/shared/infra/http/routes/index.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import costumersRouter from '@modules/customer/infra/http/routes/customers.routes';
-import ordersRouter from '@modules/order/infra/http/routes/orders.routes';
-import companiesRouter from '@modules/company/infra/http/routes/companies.routes';
-import itemsRouter from '@modules/item/infra/http/routes/items.routes';
-import sessionsCompanyRouter from '@modules/company/infra/http/routes/sessionsCompany.routes';
-import sessionsCustomerRouter from '@modules/customer/infra/http/routes/sessionsCustomer.routes';
+import costumersRouter from '@modules/customers/infra/http/routes/customers.routes';
+import ordersRouter from '@modules/orders/infra/http/routes/orders.routes';
+import companiesRouter from '@modules/companies/infra/http/routes/companies.routes';
+import itemsRouter from '@modules/items/infra/http/routes/items.routes';
+import sessionsCompanyRouter from '@modules/companies/infra/http/routes/sessionsCompany.routes';
+import sessionsCustomerRouter from '@modules/customers/infra/http/routes/sessionsCustomer.routes';
 
 const routes = Router();
 
