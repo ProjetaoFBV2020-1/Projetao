06632db6e31f9882ca5651b7b7305350bfd2bdd5
Victor Silva <victorfnss@hotmail.com>
2020-06-01 00:09:17 -0300

    início do frontend

---
+++ app/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// import SignIn from './pages/CustomerSignIn/index';
+import SignUp from './pages/CustomerSignUp/index';
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+    <>
+        <SignUp />
+        <GlobalStyle />
+    </>
+)
+
+export default App;
