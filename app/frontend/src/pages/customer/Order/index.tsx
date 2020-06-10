104a8554dc60e241b42693224268e178347e0584
Saggioro <diegosaggiorobrito@gmail.com>
2020-06-10 18:59:28 -0300

    commit do desespero

--- app/frontend/src/pages/customer/Order/index.tsx
+++ app/frontend/src/pages/customer/Order/index.tsx
@@ -24,10 +24,20 @@ interface Response {
 
 const Order: React.FC = () => {
   const { id, status, company } = useParams();
+  let pedido;
+  if(status==1){
+    pedido= "Aguardando confirmação"
+  }else if(status ==2){
+    pedido= "Pedido aceito"
+  }else if(status == 3){
+    pedido = "Saiu para entrega"
+  }
+  const [value, setValue] = useState();
   const [items, setItems] = useState<Item[]>([] as Item[]);
   useEffect(() => {
     api.get<Response>(`/orders?order_id=${id}`).then((response) => {
       setItems(response.data.order_items);
+      
     });
   }, []);
 
@@ -36,18 +46,22 @@ const Order: React.FC = () => {
       <Header />
       <Container>
         <div>
-          <div>
+          <div className="header">
             <h1>{company}</h1>
 
-            <h1>{status}</h1>
+            <h1>{pedido}</h1>
           </div>
 
           {items.map((item) => (
             <div key={item.item_id}>
-              <h1>x{item.quantity}</h1>
-              <h1>{item.name}</h1>
-
-              <h1> R$ {item.item_value}</h1>
+              <section>
+                <h1>x{item.quantity} - </h1>
+                <h1> {item.name}</h1>
+              </section>
+              <section>
+                
+                <h1> R$ {item.total_value}</h1>
+              </section>
             </div>
           ))}
         </div>
--- app/frontend/src/pages/customer/Order/styles.ts
+++ app/frontend/src/pages/customer/Order/styles.ts
@@ -1,22 +1,34 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  height: 100vh;
+  height: auto;
   display: flex;
-
-  flex-direction: column;
-  align-items: center;
-
-  align-items: stretch;
+  margin-top: 30px;
+  
   > div {
     margin: 0 auto;
+    border: solid 1px;
+    border-radius: 10px;
+    
     > div {
       display: flex;
       flex-direction: row;
+      justify-content: space-between;
       margin: 15px 0px;
       > h1 {
         margin: 0 8px;
       }
     }
+  }
+  section{
+    display: flex;
+    flex-direction: row;
+    padding: 10px
+
+    
+  }
+  .header{
+    display: flex;
+    flex-direction:column;
   }
 `;
